040ba74359dd9d1aa85d35e98413cb285b307224
Kevin <dureotkd123@naver.com>
2022-10-20 15:22:31 +0900

    useState 여러개 구현

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 const React = {
-  state: null,
+  handlerState: {
+    state: [],
+    count: 0,
+  },
   render: function () {
     const app = document.getElementById("root");
     app.innerHTML = App();
+    this.handlerState.count = 0;
   },
   useState: function (initState) {
-    if (this.state === null) {
-      this.state = initState;
+    const state = this.handlerState.state;
+    const count = this.handlerState.count;
+
+    if (state.length === count) {
+      this.handlerState.state.push(initState);
     }
 
     const setState = (newState) => {
-      this.state = newState;
+      this.handlerState.state[count] = newState;
       this.render();
     };
 
-    return [this.state, setState];
+    this.handlerState.count += 1;
+
+    return [state[count], setState];
   },
 };
+
+function Cat() {
+  let [cat, setCat] = React.useState("고양이");
+
+  window.eat = () => {
+    setCat(cat + "밥먹는중~");
+  };
+
+  return `
+    <div style='display:flex; flex-direction:column; width:200px;'>
+        <strong>${cat}</strong>
+        <button type="button" onClick="eat();" >밥먹자~</button>
+    </div>`;
+}
 
 function App() {
   let [count, setCount] = React.useState(1);
@@ -29,7 +52,9 @@ function App() {
     <div style='display:flex; flex-direction:column; width:200px;'>
         <strong>COUNT : ${count}</strong>
         <button type="button" onClick="increment();" >증가</button>
-    </div>`;
+    </div>
+    ${Cat()}
+    `;
 }
 
 React.render();
